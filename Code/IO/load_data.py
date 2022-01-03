ab3c22122217abfc5e69efad68403dd5c43a99b1
dragosconst <38582034+dragosconst@users.noreply.github.com>
2022-01-03 17:56:29 +0200

    example generation finish

--- Code/IO/load_data.py
+++ Code/IO/load_data.py
@@ -6,6 +6,9 @@ from Code.Logical.classes import ImageClasses, FaceClasses
 
 TRAIN_PATH = "antrenare/"
 DATA_PATH = "data/"
+FACES = "faces"
+FACES_LAB = "faces_labeled"
+NEG = "negative"
 NAMES = ["bart", "homer", "lisa", "marge"]
 IMGS_PER_CHAR = 1101  # holds true for all names
 CHARS = 4
@@ -24,7 +27,7 @@ type
 # check if this line belongs to the given pic
 def match_beginning_of_line(line, pic_index):
     STARTING_TEXT_LEN = len("pic_")
-    for index, char in line:
+    for index, char in enumerate(line):
         if index < STARTING_TEXT_LEN:
             continue
         number = int(line[index:index + 4])
@@ -33,7 +36,7 @@ def match_beginning_of_line(line, pic_index):
         return False
 
 def get_data_from_line(line):
-    SKIP_CHARS = len("pic_0000.jpg")
+    SKIP_CHARS = len("pic_0000.jpg ")
     relevant_line = line[SKIP_CHARS:].split(" ")
     im_class = relevant_line[4]
     if im_class == "bart":
@@ -51,38 +54,57 @@ def get_data_from_line(line):
 def load_raw_imgs():
     faces_coords_labeled = []
     faces = []
-    faces_coords = []
+    bad_faces = []
     for name in NAMES:
         imgs_path = os.path.join(TRAIN_PATH + name + "/", "*.jpg")
-        gt_path = os.path.join(TRAIN_PATH + name, '.txt')
+        gt_path = os.path.join(TRAIN_PATH, name + '.txt')
         gt_file = open(gt_path, "rt")
         gt_lines = gt_file.readlines()
         line_index = 0
         files = glob(imgs_path)
         for index, file in enumerate(files):
             img = cv.imread(file)
-            oldw, oldh = img.shape[0], img.shape[1]
+            oldw, oldh = img.shape[1], img.shape[0]
             img = cv.resize(img, (IM_WIDTH, IM_HEIGHT))
             # generate positive examples
-            pos_coords = []
             valid_pixels = [[i * IM_HEIGHT + j for j in range(IM_WIDTH)] for i in range(IM_HEIGHT)]
             valid_pixels = np.array(valid_pixels)
-            while match_beginning_of_line(gt_lines[line_index], index):
+            valid_pixels[-FACE_HEIGHT:, -FACE_WIDTH:] = -1 # so that i don't choose negative examples too close to the edges
+            while line_index < len(gt_lines) and match_beginning_of_line(gt_lines[line_index], index):
                 x1, y1, x2, y2, im_class = get_data_from_line(gt_lines[line_index])
                 fx, fy = IM_WIDTH/oldw, IM_HEIGHT/oldh # account for resizing
                 x1, y1, x2, y2 = int(x1 * fx), int(y1 * fy), int(x2 * fx), int(y2 * fy)
                 faces.append(cv.resize(img[y1:y2, x1:x2],(FACE_WIDTH, FACE_HEIGHT)))
-                faces_coords_labeled.append((x1, y1, x2, y2, im_class))
-                faces_coords.append((x1, y1, x2, y2, FaceClasses.Face.value))
-                valid_pixels[y1:y2, x1:x2] = -1
-                pos_coords.append((x1, y1, x2, y2))
+                cv.imwrite(DATA_PATH + "faces/" + str(index) + ".jpg", cv.resize(img[y1:y2, x1:x2],(FACE_WIDTH, FACE_HEIGHT)))
+                faces_coords_labeled.append((len(faces) - 1, x1, y1, x2, y2, im_class))
+                # faces_coords.append((len(faces) - 1, x1, y1, x2, y2, FaceClasses.Face.value))
+                valid_pixels[max(y1 - FACE_HEIGHT, 0):y2, max(x1 - FACE_WIDTH, 0):x2] = -1 # make sure we won't choose negative examples that contain faces
                 line_index += 1
 
             # generate negative examples
-            # first idea: randomly choose segments that are outside the bounds of any faces
-            # this could be quite slow, due to having to compute all valid pixels for each image
+            # i think they only make sense for facial detection, so i'm only going to generate negative examples for that
             for neg in range(NEG_EX_PER_IMG):
                 vp = valid_pixels[valid_pixels > -1]
+
+                if len(vp) == 0:
+                    print(f"Only found {neg} negative examples for image {index}, from {name} dataset...")
+                    break
+
+                index = np.random.randint(0, len(vp), size=1)
+                y, x = int(vp[index] / IM_HEIGHT), int(vp[index] % IM_HEIGHT)
+                bad_faces.append(img[y:y+FACE_HEIGHT, x:x+FACE_WIDTH])
+                cv.imwrite(DATA_PATH + "badfaces/" + str(index) + "_" + str(neg) + ".jpg", img[y:y+FACE_HEIGHT, x:x+FACE_WIDTH])
+                # bad_faces_coords.append((len(faces) - 1, x, y, x + FACE_WIDTH, y + FACE_HEIGHT))
+                valid_pixels[y, x] = -1 # unlike with faces, overlapping negative examples shouldn't be too much of a problem,
+                                        # but, at the same time, generating the same negative example over and over again isn't
+                                        # probably the greatest idea
+
+    faces = np.asarray(faces)
+    # faces_coords = np.asarray(faces_coords)
+    faces_coords_labeled = np.asarray(faces_coords_labeled)
+    bad_faces = np.asarray(bad_faces)
+    # bad_faces_coords = np.asarray(bad_faces_coords)
+    return faces, faces_coords_labeled, bad_faces
 
 
 
@@ -92,10 +114,15 @@ def load_raw_imgs():
 This will generate both positive and negative examples. I'm using only one function since it should save some time
 to only open the files once, only read 
 """
-def generate_examples():
-    for name in NAMES:
-        imgs_path = os.path.join(TRAIN_PATH + name + "/", "*.jpg")
-        files = glob(imgs_path)
-        for index, file in enumerate(files):
-            img = cv.imread(file)
-
+def get_examples():
+    faces_npy = os.path.join(DATA_PATH, "faces.npy")
+    if os.path.exists(faces_npy):
+        faces = np.load(DATA_PATH + FACES + ".npy", allow_pickle=True)
+        faces_labels = np.load(DATA_PATH + FACES_LAB + ".npy", allow_pickle=True)
+        negatives = np.load(DATA_PATH + NEG + ".npy", allow_pickle=True)
+    else:
+        faces, faces_labels, negatives = load_raw_imgs()
+        np.save(os.path.join(DATA_PATH, FACES + ".npy"), faces)
+        np.save(os.path.join(DATA_PATH, FACES_LAB + ".npy"), faces_labels)
+        np.save(os.path.join(DATA_PATH, NEG + ".npy"), negatives)
+    return faces, faces_labels, negatives
---
+++ main.py
@@ -0,0 +1,7 @@
+from Code.IO.load_data import get_examples
+
+def main():
+    get_examples()
+
+if __name__ == "__main__":
+    main()
